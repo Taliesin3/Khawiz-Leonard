6d5535c888805a3564b95080e84b6e0784cc2b52
nathanhmiles <60706094+nathanhmiles@users.noreply.github.com>
2020-12-05 17:48:08 +0000

    Updated env variables for heroku

--- Khawiz/settings.py
+++ Khawiz/settings.py
@@ -11,12 +11,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['khawiz-leonard.herokuapp.com']
 
 
 # Application definition
